3ae9422859f7588c2e3a2261308ac25f3e88bac4
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-06-15 11:19:04 +0200

    added new names to parameters

--- src/movies.js
+++ src/movies.js
@@ -4,8 +4,8 @@ import { movies } from "./data.js"
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
-function getAllDirectors(moviesArray) {
-    const movieDirectors = moviesArray.map(movie => {
+function getAllDirectors(movies) {
+    const movieDirectors = movies.map(movie => {
         return movie.director;
     });
     return movieDirectors;
@@ -19,12 +19,10 @@ console.log(getAllDirectors(movies));
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 
-function howManyMovies(array) {
-    const dramaStevenSpielbergMovies = array.filter((directorDrama) => {
+function howManyMovies(movies) {
+    const dramaStevenSpielbergMovies = movies.filter((directorDrama) => {
       return (
-        directorDrama.director.toLowerCase() === "steven spielberg" &&
-        directorDrama.genre.includes("Drama") 
-      );
+        directorDrama.director === "Steven Spielberg" && directorDrama.genre.includes("Drama") );
     });
     return dramaStevenSpielbergMovies
   }
@@ -44,21 +42,22 @@ console.log(finalScore);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
-// function scoresDrama(movies) {
-    
-//     const dramaMovies = movies.genre.filter(movie => movie.genre === 'Drama');
-   
-//     const dramaTotalScore = dramaMovies.reduce((acc, movie) => acc + movie.score, 0);
-   
-//     const averageDramaScore = dramaTotalScore / dramaMovies.length;
+function dramaMoviesScore(movies) {
 
-//     return averageDramaScore;
-// }
+    const dramaMovies = movies.filter(movie => {
+        return movie.genre.includes("Drama");
+    });
 
-// console.log(scoresDrama(movies));
+    const averageDramaSum = dramaMovies.reduce((acc, movie) => {
+        return acc + (movie.score || 0);  
+    }, 0);
 
+    let averageDrama = (averageDramaSum / dramaMovies.length).toFixed(2);
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+    return `The average score of drama movies is ${averageDrama}`;
+}
+
+console.log(dramaMoviesScore(movies));
 
 
 function orderByYear(movies) {
